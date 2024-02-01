0871ee509074ff69ad080c73e0b695ae635d7e92
Soe Moe <hi@soe.moe>
2024-02-01 19:54:02 +0630

    fix (ui): post page style

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -109,7 +109,7 @@ export default async function PostPage({ params }: PostPageProps) {
             Published on {formatDate(post.date)}
           </time>
         )}
-        <h1 className="mt-2 inline-block font-heading text-4xl leading-tight lg:text-5xl">
+        <h1 className="my-2 inline-block font-heading text-4xl lg:text-5xl">
           {post.title}
         </h1>
       </div>
