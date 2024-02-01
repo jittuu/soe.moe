4c01debe8d5cd11034d0728cb40460a9f6bd4cc1
Soe Moe <hi@soe.moe>
2024-02-01 22:48:46 +0630

    fix: footer style

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -6,30 +6,7 @@ import { ModeToggle } from "./mode-toggle";
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer className={cn(className)}>
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
-        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <p className="text-center text-sm leading-loose md:text-left">
-            Built by Soe Moe. Hosted on{" "}
-            <a
-              href="https://pages.cloudflare.com/"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              Cloudflare Pages
-            </a>
-            . The source code is available on{" "}
-            <a
-              href="https://github.com/jittuu/soe.moe"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              GitHub
-            </a>
-            .
-          </p>
-        </div>
+      <div className="container flex justify-center sm:justify-end py-10">
         <ModeToggle />
       </div>
     </footer>
