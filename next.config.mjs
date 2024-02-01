a1396d7b5305f44342e1eecce135aeb30bb9a2d5
Soe Moe <hi@soe.moe>
2024-02-01 18:58:57 +0630

    chore: disable image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import { withContentlayer } from "next-contentlayer";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withContentlayer(nextConfig);
