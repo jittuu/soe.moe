416f27dc8be627b276a6c7d32e5f33fba289d308
Soe Moe <hi@soe.moe>
2024-02-01 18:31:47 +0630

    chore: set output as export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+};
 
 export default withContentlayer(nextConfig);
